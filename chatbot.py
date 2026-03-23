c7e98b7c7a3be7c1dcf09c508cb9ec031c4c0d3c
AniketPrajapati <aniketprajapati168@gmail.com>
2026-03-23 14:28:06 +0530

    fix: added debug for API key

--- chatbot.py
+++ chatbot.py
@@ -3,11 +3,13 @@ from dotenv import load_dotenv
 
 # load api key from .env
 load_dotenv()
+print("API KEY FOUND:", os.getenv("GOOGLE_API_KEY") is not None)
 
 # create AI model
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash"
 )
+
 
 # function to get response
 def get_response(user_input):
